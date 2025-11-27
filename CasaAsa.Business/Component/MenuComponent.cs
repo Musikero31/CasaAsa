b677a98e0d3b19ebddfa7f23347dd7eae7fce3e1
Ryan <ryan.aimel.estandarte@gmail.com>
2025-11-26 23:03:53 -0600

    Create a menu component

---
+++ CasaAsa.Business/Component/MenuComponent.cs
@@ -0,0 +1,23 @@
+﻿using CasaAsa.Data;
+using CasaAsa.Data.Models;
+
+namespace CasaAsa.Business.Component
+{
+    public class MenuComponent
+    {
+        private readonly IRepository<MenuDetail> _menuDetailRepository;
+        private readonly IRepository<MenuCategory> _menuCategoryRepository;
+
+        public MenuComponent(IRepository<MenuDetail> menuDetailRepository,
+                             IRepository<MenuCategory> menuCategoryRepository)
+        {
+            _menuDetailRepository = menuDetailRepository;
+            _menuCategoryRepository = menuCategoryRepository;
+        }
+
+        //public async Task<List<MenuCategories>> GetMenuCategoriesAsync()
+        //{
+        //    return await _menuCategoryRepository.GetAllAsync();
+        //}
+    }
+}
