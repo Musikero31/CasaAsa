8376eec2f148c7343a1bf099a204578e5f9d278d
Ryan <ryan.aimel.estandarte@gmail.com>
2025-11-30 18:57:05 -0600

    Added login to swagger gen.

--- CasaAsa.API/Program.cs
+++ CasaAsa.API/Program.cs
@@ -5,6 +5,7 @@ using CasaAsa.Data.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -49,14 +50,50 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddAutoMapper(x => x.AddMaps(typeof(EmptyProfileMarker).Assembly));
 builder.Services.AddComponents();
 builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CasaAsa API",
+        Version = "v1"
+    });
+
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter JWT Bearer token **_without_** the word Bearer.\n\nExample:\n12345abcdef",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+
+    c.AddSecurityDefinition("Bearer", securitySchema);
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            securitySchema,
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CasaAsa API v1");
+    });
 }
 
 app.UseHttpsRedirection();
