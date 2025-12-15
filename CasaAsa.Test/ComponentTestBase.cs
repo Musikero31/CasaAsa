21bab178918f3f7bdbf35029ff3add7c0fd66c81
Ryan <ryan.aimel.estandarte@gmail.com>
2025-12-15 16:13:05 -0600

    Test Project
    Added a test project for future development.

---
+++ CasaAsa.Test/ComponentTestBase.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaAsa.Test
+{
+    public abstract class ComponentTestBase
+    {
+        public IMapper SetupMapping<TProfile>(ILogger<TProfile> logger)
+    where TProfile : Profile
+        {
+            var profile = (TProfile)Activator.CreateInstance(typeof(TProfile), logger)!;
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(profile);
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ CasaAsa.Test/Components/AdminComponentTests.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using CasaAsa.Business.Component;
+using CasaAsa.Business.Profiles;
+using CasaAsa.Data.Repository;
+using DataModel = CasaAsa.Data.Models;
+using Moq;
+using Microsoft.Extensions.Logging;
+
+namespace CasaAsa.Test.Components
+{
+    public class AdminComponentTests
+    {
+        private readonly Mock<IRepository<DataModel.LockOrder>> _mockRepo;
+        private readonly IMapper _mapper;
+        private readonly IAdminComponent _component;
+        private readonly ILoggerFactory _logger;
+
+        public AdminComponentTests()
+        {
+            // 1. Mock repository
+            _mockRepo = new Mock<IRepository<DataModel.LockOrder>>();
+
+            // 2. Configure AutoMapper with your test profile
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<LockSettingProfile>();
+            }, _logger).CreateMapper();
+
+            // 3. Instantiate component under test
+            _component = new AdminComponent(_mockRepo.Object, _mapper!);
+        }
+
+        [Fact]
+        public void GetLatestLockOrder_should_return_latest_lock_date()
+        {
+
+        }
+    }
+}
