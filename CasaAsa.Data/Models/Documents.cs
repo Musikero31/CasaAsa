d2d0dcc30197e445acd31d368b35d5cc50008df7
Ryan <ryan.aimel.estandarte@gmail.com>
2026-02-26 16:53:14 -0600

    Allow upload photos.
    * Created a new table Documents
    * Created relationship with the Menu Detail table.

---
+++ CasaAsa.Data/Models/Documents.cs
@@ -0,0 +1,18 @@
+﻿namespace CasaAsa.Data.Models
+{
+    public class Documents : IAuditEntity
+    {
+        public int Id { get; set; }
+        public byte[] DocumentFile { get; set; }
+        public string DocumentName { get; set; }
+        public string DocumentType { get; set; }
+        public string FileType { get; set; }
+        public bool ActiveStatus { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+        public ICollection<MenuDetail> MenuDetails { get; set; }
+    }
+}
