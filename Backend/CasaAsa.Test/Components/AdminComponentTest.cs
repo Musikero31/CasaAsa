ec44217286a7e91726bbf7cd744de4a872ea5c7a
Ryan <ryan.aimel.estandarte@gmail.com>
2026-04-22 18:23:10 -0500

    Setup a unit test with one test sample

--- Backend/CasaAsa.Test/Components/AdminComponentTest.cs
+++ Backend/CasaAsa.Test/Components/AdminComponentTest.cs
@@ -1,41 +1,73 @@
-﻿using AutoMapper;
+﻿using CasaAsa.Data.Repository;
+using DataModel = CasaAsa.Data.Models;
+using AutoMapper;
 using CasaAsa.Business.Component.Administration;
+using NSubstitute;
+using CasaAsa.Test.Mappings;
 using CasaAsa.Business.Component.Administration.Authentication;
-using CasaAsa.Data.Repository;
-using Castle.Core.Logging;
 using Microsoft.Extensions.Logging;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentAssertions;
 
 namespace CasaAsa.Test.Components
 {
     public class AdminComponentTest
     {
-        private readonly Mock<IRepository<Data.Models.LockOrder>> _lockOrderRepoMock;
-        private readonly AdminComponent _adminCompMock;
-        private readonly Mock<IMapper> _mapperMock;
-        private readonly Mock<IAuthenticationService> _authSvcMock;
-        private readonly Mock<IAddressComponent> _addressCompMock;
-        private readonly Mock<ILogger<AdminComponent>> _loggerAdminCompMock;
+        private readonly IRepository<DataModel.LockOrder> _lockRepo;
+        private readonly IMapper _mapper;
+        private readonly IAdminComponent _adminComp;
+        private readonly IAuthenticationService _authSvc;
+        private readonly IAddressComponent _addressComp;
+        private readonly ILogger<AdminComponent> _logger;
 
         public AdminComponentTest()
         {
-            _lockOrderRepoMock = new Mock<IRepository<Data.Models.LockOrder>>();
-            _mapperMock = new Mock<IMapper>();
-            _authSvcMock = new Mock<IAuthenticationService>();
-            _addressCompMock = new Mock<IAddressComponent>();
-            _loggerAdminCompMock = new Mock<ILogger<AdminComponent>>();
+            _lockRepo = Substitute.For<IRepository<DataModel.LockOrder>>();
+            _authSvc = Substitute.For<IAuthenticationService>();
+            _addressComp = Substitute.For<IAddressComponent>();
+            _logger = Substitute.For<ILogger<AdminComponent>>();
 
-            _adminCompMock = new AdminComponent(_lockOrderRepoMock.Object,
-                                                _mapperMock.Object,
-                                                _authSvcMock.Object,
-                                                _addressCompMock.Object,
-                                                _loggerAdminCompMock.Object);
+            var mapping = new TestMappingProfiles();
+            _mapper = mapping.Mapper;
 
+            _adminComp = new AdminComponent(_lockRepo, _mapper, _authSvc, _addressComp, _logger);
+        }
+
+        [Fact]
+        public async Task GetLatestLockOrderAsync_Should_Return_Latest_Order_Date()
+        {
+            // Arrange
+            var lockOrders = new List<DataModel.LockOrder>
+            {
+                new DataModel.LockOrder
+                {
+                    Id = 3,
+                    LockDate = DateOnly.FromDateTime(DateTime.Now),
+                    ActiveStatus = true
+                },
+                new DataModel.LockOrder
+                {
+                    Id = 1,
+                    LockDate = DateOnly.FromDateTime(DateTime.Now.AddMonths(-2)),
+                    ActiveStatus = false
+                },
+                new DataModel.LockOrder
+                {
+                    Id = 2,
+                    LockDate = DateOnly.FromDateTime(DateTime.Now.AddDays(-10)),
+                    ActiveStatus = false
+                }
+            };
+
+            _lockRepo.GetAllAsync().Returns(lockOrders);
+
+            // Act
+            var result = await _adminComp.GetLatestLockOrderAsync();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.ActiveStatus.Should().BeTrue();
+            result.LockDate.Should().Be(DateOnly.FromDateTime(DateTime.Now));
+            
         }
     }
 }
---
+++ Backend/CasaAsa.Test/Mappings/TestMappingProfiles.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using CoreProfiles = CasaAsa.Business.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace CasaAsa.Test.Mappings
+{
+    public class TestMappingProfiles
+    {
+        public IMapper Mapper { get; set; }
+
+        public TestMappingProfiles()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {                
+                cfg.AddProfile<CoreProfiles.AddressMapProfile>();
+                cfg.AddProfile<CoreProfiles.CustomerMapProfile>();
+                cfg.AddProfile<CoreProfiles.DocumentsMapProfiles>();
+                cfg.AddProfile<CoreProfiles.LockSettingMapProfile>();
+                cfg.AddProfile<CoreProfiles.MenuMapProfiles>();
+
+            }, NullLoggerFactory.Instance);
+
+            Mapper = config.CreateMapper();
+        }
+    }
+}
