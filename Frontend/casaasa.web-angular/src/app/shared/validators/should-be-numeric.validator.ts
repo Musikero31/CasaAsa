69b2cd7bddee2d7f6f1d72bd0b49bae497487f99
Ryan <ryan.aimel.estandarte@gmail.com>
2026-07-25 11:35:18 -0500

    Changes from the code review
    * Split the styling for hte normal button and the hover.
    * Removed the duplicate Register in the route component
    * Used regex to check if the value entered is a number or not.

--- Frontend/casaasa.web-angular/src/app/shared/validators/should-be-numeric.validator.ts
+++ Frontend/casaasa.web-angular/src/app/shared/validators/should-be-numeric.validator.ts
@@ -2,8 +2,9 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function shouldBeNumeric() : ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-        let value = control.value;
-        if (value && isNaN(value)) {
+        const value = control.value?.trim();
+        const isNumeric = /^\d+$/.test(String(value));
+        if (value !== null && value !== undefined && !isNumeric) {
             return { notNumeric: true };
         }
         return null;
