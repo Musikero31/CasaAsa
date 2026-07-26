896705088720457c153729b650951f590549ae1e
Ryan <ryan.aimel.estandarte@gmail.com>
2026-07-25 21:38:38 -0500

    Confirm User Page
    * Added new route for the Confirm page
    * Created notification service.
    * Changed the return value for both error and Ok messages.
    * Changed the UI login to display the message.
    * Revised the confirmEmail service to use post and pass it via the object.
    * Revised the endpoint Confirm to use the verb post instead of get.
    * Created the ConfirmUserModel model.
    * Fixed the confirm button in the email to be the same as with the pages.
    * Fixes for the possible empty UserID and/or Token

---
+++ Backend/CasaAsa.API/Areas/Administrator/Models/ConfirmUserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CasaAsa.API.Areas.Administrator.Models
+{
+    public class ConfirmUserViewModel
+    {
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public string Token { get; set; } = string.Empty;
+    }
+}
--- Frontend/casaasa.web-angular/src/app/features/auth/confirmation/confirmation.ts
+++ Frontend/casaasa.web-angular/src/app/features/auth/confirmation/confirmation.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthApiService } from '../../../shared/services/features/auth-api.service';
+import { NotificationService } from '../../../core/services/notification.service';
+import { ConfirmUserModel } from '../../../shared/models/authentication/confirmUser.model';
 
 @Component({
   selector: 'app-confirmation',
@@ -6,4 +10,55 @@ import { Component } from '@angular/core';
   templateUrl: './confirmation.html',
   styleUrl: './confirmation.scss',
 })
-export class Confirmation {}
+export class Confirmation implements OnInit{  
+  private _route = inject(ActivatedRoute);
+  private _authAPI = inject(AuthApiService);
+  private _router = inject(Router);
+  private _notificationSvc = inject(NotificationService);
+
+  userId: string | null = null;
+  token: string | null = null;
+  message: string | null = null;
+
+  ngOnInit(): void {
+    this._route.queryParamMap.subscribe(params => {
+      this.userId = params.get('userId') ?? "";
+      this.token = params.get('token') ?? "";
+
+      if (!this.userId || !this.token) {
+        this._notificationSvc.showMessage('Invalid confirmation link.');
+        this._router.navigate(['/login']);
+        return;
+      }
+
+      let confirmUser: ConfirmUserModel = {
+        userId: this.userId,
+        token: this.token
+      };
+
+      this._authAPI.confirmEmail(confirmUser).subscribe({
+        next: (response) => {
+          this._notificationSvc.showMessage(response.message);
+          this._router.navigate(['/login']);
+        },
+        error: (error) => {
+          let message = 'Error confirming email.';
+
+          if (typeof error.error === 'string') {
+            message = error.error;
+          } 
+          else if (error.error?.message) {
+            message = error.error.message;
+          } 
+          else if (error.message) {
+            message = error.message;
+          }
+
+          console.error('Error confirming email:', error);
+          this._notificationSvc.showMessage(message);
+          this._router.navigate(['/login']);
+        }
+      });
+    });
+  }
+}
